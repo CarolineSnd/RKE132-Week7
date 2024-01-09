0b025d89470eac0d0c63d043736a68ab30c226b1
Caroline Sünd <carolinesund@Carolines-MacBook-Air.local>
2024-01-09 19:14:34 +0200

    seitsmes

---
+++ ConvertTo/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Take your choice: Celcius or Fahrenheit - F/C :");
+char userChoice = Char.Parse(Console.ReadLine());
+
+
+int userInput = Int32.Parse(Console.ReadLine());
+
+switch (userChoice)
+{
+    case 'F':
+        ConvertToCelcius(userInput);
+        break;
+    case 'C':
+        ConvertToFahrenheit(userInput);
+        break;
+}
+
+
+static void ConvertToCelcius(int celsius)
+{
+    Console.WriteLine($"{(celsius*9)/5+32}");
+}
+
+static void ConvertToFahrenheit(int fahrenheit)
+        {
+            Console.WriteLine($"{(fahrenheit - 32 * (5 / 9))}");
+        }
